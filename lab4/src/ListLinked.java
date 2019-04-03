ccbd260364d982503c4d3b804978e6b87c44b92c
systemvaz <alex@systemvaz.net>
2019-04-04 00:15:41 +0800

    update

--- lab4/src/ListLinked.java
+++ lab4/src/ListLinked.java
@@ -1,3 +1,8 @@
+/**
+ * @author Alexander Varano della Vergiliana
+ * @version 1.0
+ */
+
 import CITS2200.*;
 
 public class ListLinked implements List
@@ -6,6 +11,10 @@ public class ListLinked implements List
 	private Link after;
 	public CITS2200.WindowLinked window;
 	
+	/**
+	 * Constructor initialising the Linked List ADT, setting up the before first and after last elements.
+	 * Setting up a new window object and assigning it to the before first position.
+	 */
 	public ListLinked()
 	{
 		after = new Link(null, null);
@@ -14,19 +23,29 @@ public class ListLinked implements List
 		beforeFirst(window);
 	}
 
-
+	/**
+	 * Sets the window to the after last position.
+	 * @param window Takes the window object as input
+	 */
 	public void afterLast(CITS2200.WindowLinked window) 
 	{
 		window.link = after;
 	}
 
-
+	/**
+	 * Sets the window to the before first position
+	 * @param window Takes the window object as input
+	 */
 	public void beforeFirst(CITS2200.WindowLinked window) 
 	{
 		window.link = before;
 	}
 
-
+	/**
+	 * Deletes the list item underneath the window
+	 * @param window Takes the window object as input
+	 * @throws OutOfBounds If the window is currently over the before first or after last position
+	 */
 	public Object delete(CITS2200.WindowLinked window) throws OutOfBounds 
 	{
 		if(!isBeforeFirst(window) && !isAfterLast(window))
@@ -46,7 +65,11 @@ public class ListLinked implements List
 		}
 	}
 
-
+	/**
+	 * Returns the current item underneath the window
+	 * @param window Takes the window object as input
+	 * @throws OutOfBounds If the window is currently over the before first or after last position
+	 */
 	public Object examine(CITS2200.WindowLinked window) throws OutOfBounds 
 	{
 		if(!isBeforeFirst(window) && !isAfterLast(window))
@@ -59,6 +82,12 @@ public class ListLinked implements List
 		}
 	}
 
+	/**
+	 * Creates a new link after current window position and inserts user supplied object
+	 * @param window Take the window object as input
+	 * @param e User supplied Object to be inserted into new list element
+	 * @throws OutOfBounds If window is over the after last position
+	 */
 	public void insertAfter(Object e, CITS2200.WindowLinked window) throws OutOfBounds 
 	{
 		if(!isAfterLast(window))
@@ -72,7 +101,12 @@ public class ListLinked implements List
 		}
 	}
 
-
+	/**
+	 * Creates a new link before current window position and inserts user supplied object
+	 * @param window Take the window object as input
+	 * @param e User supplied Object to be inserted into new list element
+	 * @throws OutOfBounds If window is over the before first position
+	 */
 	public void insertBefore(Object e, CITS2200.WindowLinked window) throws OutOfBounds
 	{
 		if(!isBeforeFirst(window))
@@ -91,25 +125,40 @@ public class ListLinked implements List
 		}	
 	}
 
-
+	/**
+	 * Specifies whether the window is over the after last position
+	 * @param window Take the window object as input
+	 * @return boolean True is over the after last position, otherwise false.
+	 */
 	public boolean isAfterLast(CITS2200.WindowLinked window)
 	{
 		return(window.link == after);
 	}
 
-
+	/**
+	 * Specifies whether the window is over the before first position
+	 * @param window Take the window object as input
+	 * @return boolean True is over the before first position, otherwise false.
+	 */
 	public boolean isBeforeFirst(CITS2200.WindowLinked window)
 	{
 		return(window.link == before);
 	}
 
-
+	/**
+	 * Specifies whether the list is empty.
+	 * @return boolean True if empty, otherwise false.
+	 */
 	public boolean isEmpty() 
 	{
 		return(before.successor == after);
 	}
 
-
+	/**
+	 * Moves the window to the next item in the list
+	 * @param window Takes the window object as input
+	 * @throws OutOfBounds If the window is over the after last position.
+	 */
 	public void next(CITS2200.WindowLinked window) throws OutOfBounds
 	{
 		if(!isAfterLast(window))
@@ -123,7 +172,11 @@ public class ListLinked implements List
 		
 	}
 
-
+	/**
+	 * Moves the window to the previous item in the list
+	 * @param window Takes the window object as input
+	 * @throws OutOfBounds If the window is over the before first position.
+	 */
 	public void previous(CITS2200.WindowLinked window) throws OutOfBounds
 	{
 		if(!isBeforeFirst(window))
@@ -144,7 +197,13 @@ public class ListLinked implements List
 		}
 	}
 
-
+	/**
+	 * Replaces the item underneath the window with the user supplied input
+	 * @param e User supplied Object to replace with
+	 * @param window Take the window object as input
+	 * @return The item that has been replaced by the new object.
+	 * @throws OutOfBounds If the window is in the before first or after last position.
+	 */
 	public Object replace(Object e, CITS2200.WindowLinked window) throws OutOfBounds
 	{
 		if(!isBeforeFirst(window) && !isAfterLast(window))
